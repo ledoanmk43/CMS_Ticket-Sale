de325f045b22c3df261c2f1eecbf9832cd8f8685
Lê Đoàn <52238710+ledoanmk43@users.noreply.github.com>
2022-04-01 22:25:36 +0700

    Update UI
    chart layout

---
+++ .history/cms-app/src/layout/Main/Dashboard_20220401105617.tsx
@@ -0,0 +1,46 @@
+import { DatePicker } from 'antd'
+import moment from 'moment'
+import React, { useRef, useEffect, useState } from 'react'
+import Calendar from '../Components/Calendar'
+
+export interface IDashboardProps {}
+
+const Dashboard: React.FunctionComponent<IDashboardProps> = (props) => {
+  const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false)
+
+  return (
+    <div className='main pr-6rem'>
+      <div className='container'>
+        <p className='title_xl'> Thống kê</p>
+        <div className='revenue'>
+          <div className='section'>
+            <p className='title'>Doanh thu</p>
+            <DatePicker
+              onClick={() => setIsCalendarOpen(!isCalendarOpen)}
+              style={{ position: 'relative' }}
+              defaultValue={moment(new Date(), 'DD MMM, YYYY')}
+              defaultPickerValue={moment(new Date(), 'DD MMM, YYYY')}
+              format={'DD/MM/YYYY'}
+              allowClear={false}
+            />
+            {isCalendarOpen && (
+              <Calendar
+                onClickOutside={() => {
+                  setIsCalendarOpen(false)
+                }}
+              />
+            )}
+          </div>
+          <div className='section col-start'>
+            <p className='sm-title'>Tổng doanh thu theo tuần</p>
+            <p className='sum-revenue'>
+              525.145.000 <span className='currency'>đồng</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ .history/cms-app/src/layout/Main/Dashboard_20220401212716.tsx
@@ -0,0 +1,104 @@
+import { DatePicker } from 'antd'
+import moment from 'moment'
+import React, { useRef, useEffect, useState } from 'react'
+import Calendar from '../Components/Calendar'
+import { Area } from '@ant-design/plots'
+
+export interface IDashboardProps {}
+
+const Dashboard: React.FunctionComponent<IDashboardProps> = (props) => {
+  const [isCalendarSumOpen, setIsCalendarSumOpen] = useState<boolean>(false)
+  const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false)
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    asyncFetch()
+  }, [])
+
+  const asyncFetch = () => {
+    fetch(
+      'https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json'
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log('fetch data failed', error)
+      })
+  }
+  const config = {
+    data,
+    xField: 'timePeriod',
+    yField: 'value',
+    xAxis: {
+      range: [0, 1],
+    },
+    color: '#a8ddb5',
+  }
+
+  return (
+    <div className='main col pr-6rem'>
+      <div className='container'>
+        <p className='title_xl'> Thống kê</p>
+        <div className='revenue'>
+          <div className='section'>
+            <p className='title'>Doanh thu</p>
+            <DatePicker
+              onClick={() => setIsCalendarSumOpen(!isCalendarSumOpen)}
+              style={{ position: 'relative' }}
+              defaultValue={moment(new Date(), 'DD MMM, YYYY')}
+              defaultPickerValue={moment(new Date(), 'DD MMM, YYYY')}
+              format={'MMM, YYYY'}
+              allowClear={false}
+            />
+            {isCalendarSumOpen && (
+              <Calendar
+                onClickOutside={() => {
+                  setIsCalendarSumOpen(false)
+                }}
+              />
+            )}
+          </div>
+          <div className='chart'>
+            <Area {...config} />
+          </div>
+          <div className='section col-start'>
+            <p className='sm-title'>Tổng doanh thu theo tuần</p>
+            <p className='sum-revenue'>
+              525.145.000 <span className='currency'>đồng</span>
+            </p>
+          </div>
+          <div className='section row h-auto'>
+            <DatePicker
+              onClick={() => setIsCalendarOpen(!isCalendarOpen)}
+              style={{ position: 'relative' }}
+              defaultValue={moment(new Date(), 'DD MMM, YYYY')}
+              defaultPickerValue={moment(new Date(), 'DD MMM, YYYY')}
+              format={'MMM, YYYY'}
+              allowClear={false}
+            />
+            {isCalendarOpen && (
+              <Calendar
+                onClickOutside={() => {
+                  setIsCalendarOpen(false)
+                }}
+              />
+            )}
+            <div className='col-start h-auto mt-5rem'>
+              <div className='hint row h-auto'>
+                <div className='cube used '></div>
+                <p>Vé đã sử dụng</p>
+              </div>
+              <div className='hint row h-auto'>
+                <div className='cube ready'></div>
+                <p>Vé chưa sử dụng</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
